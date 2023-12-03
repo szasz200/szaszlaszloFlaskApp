b917ad32a599051862c260aa478eb4c3c215fb9f
szasz200 <73BFVebKLbL3JS7t>
2023-12-03 11:31:46 +0100

    Add DB.

--- models.py
+++ models.py
@@ -4,11 +4,11 @@ from app import db
 
 
 class Temperature(db.Model):
-    __tablename__ = 'restaurant'
+    __tablename__ = 'temperature'
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
     time = Column(String(50))
-    temperature = Column(String(6))
+    temperature = Column(String(10))
 
     def __str__(self):
         return self.name
